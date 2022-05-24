4471408080d3ec7e12823ff467d6ff8561feca26
2021-2022-projectone-vinnikalessia <alessia.vinnik@student.howest.be>
2022-05-24 13:50:51 +0200

    project 1 en "lampen" uit elkaar houden

---
+++ project/backend/app_v1.py
@@ -0,0 +1,93 @@
+from repositories.DataRepository import DataRepository
+from flask_socketio import SocketIO, emit, send
+from helpers.klasseknop import Button
+from flask import Flask, jsonify
+from selenium import webdriver
+from logging import exception
+from flask_cors import CORS
+import threading
+import spidev
+import time
+from RPi import GPIO
+
+
+# de joystick
+# deze hangen aan de mcp
+y_as = 0
+x_as = 1
+
+
+# de sw van de joystick
+sw = 5
+
+# teller aantal keer sw ingedrukt
+teller = 0
+
+# de spi
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 10 ** 5
+
+def setup():
+    print("setup")
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(sw, GPIO.IN, GPIO.PUD_UP)
+    GPIO.add_event_detect(sw, GPIO.FALLING, callback_knop, bouncetime = 100)
+
+def callback_knop(pin):
+    global teller
+    print(teller)
+    teller += 1
+    print("Button pressed {0}\n".format(teller))
+
+
+def readChannel(channel):
+    val = spi.xfer2([1,(8+channel)<<4,0])
+    data = ((val[1] << 8) + val[2])
+    return data
+
+# Code voor Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'geheim!'
+socketio = SocketIO(app, cors_allowed_origins="*", logger=False,
+                    engineio_logger=False, ping_timeout=1)
+
+CORS(app)
+
+
+@socketio.on_error()        # Handles the default namespace
+def error_handler(e):
+    print(e)
+
+@app.route('/')
+def hallo():
+    return "Server is running, er zijn momenteel geen API endpoints beschikbaar."
+
+@socketio.on('connect')
+def initial_connection():
+    print('A new client connect')
+    # # Send to the client!
+    # vraag de status op van de lampen uit de DB
+    status = DataRepository.read_status_lampen()
+    emit('B2F_status_lampen', {'lampen': status}, broadcast=True)
+
+try:
+    setup()
+    while True:
+        sw_val = readChannel(sw)
+        print(f"dit is de sw: {sw_val}")
+        x_val = readChannel(x_as)
+        print(f"dit is de x: {x_val}")
+        y_val = readChannel(y_as)
+        print(f"dit is de y: {y_val}\n")
+        time.sleep(0.5)
+except Exception as e:
+    print(e)
+finally:
+    print("cleanup pi")
+    spi.close()
+    GPIO.cleanup
+
+
