528a420f062819787185cf74158ffbfbada9e802
2021-2022-projectone-vinnikalessia <alessia.vinnik@student.howest.be>
2022-05-25 13:26:48 +0200

    frontend bezig

--- project/frontend/script/app.js
+++ project/frontend/script/app.js
@@ -1,126 +1,47 @@
 "use strict";
-const lanIP = `${window.location.hostname}:5000`;
-const socket = io(`http://${lanIP}`);
-console.info(lanIP)
 
-// const clearClassList = function (el) {
-//   el.classList.remove("c-room--wait");
-//   el.classList.remove("c-room--on");
-// };
+// #region ***  DOM references                           ***********
+let htmlDevice, htmlJoystick
+// #endregion
 
-// const listenToUI = function () {
-//   const knoppen = document.querySelectorAll(".js-power-btn");
-//   for (const knop of knoppen) {
-//     knop.addEventListener("click", function () {
-//       const id = this.dataset.idlamp;
-//       let nieuweStatus;
-//       if (this.dataset.statuslamp == 0) {
-//         nieuweStatus = 1;
-//       } else {
-//         nieuweStatus = 0;
-//       }
-//       //const statusOmgekeerd = !status;
-//       clearClassList(document.querySelector(`.js-room[data-idlamp="${id}"]`));
-//       document.querySelector(`.js-room[data-idlamp="${id}"]`).classList.add("c-room--wait");
-//       socket.emit("F2B_switch_light", { lamp_id: id, new_status: nieuweStatus });
-//     });
-//   }
-// };
+// #region ***  Callback-Visualisation - show___         ***********
+const showDevices = function(jsonObject){
+  try{
+    console.info(jsonObject)
+  }catch(err){
+    console.error(err)
+  }
+}
+// #endregion
 
-// const listenToSocket = function () {
-//   socket.on("connected", function () {
-//     console.log("verbonden met socket webserver");
-//   });
+// #region ***  Callback-No Visualisation - callback___  ***********
+// #endregion
 
-//   socket.on("B2F_status_lampen", function (jsonObject) {
-//     console.log("alle lampen zijn automatisch uitgezet");
-//     console.log("Dit is de status van de lampen");
-//     console.log(jsonObject);
-//     for (const lamp of jsonObject.lampen) {
-//       const room = document.querySelector(`.js-room[data-idlamp="${lamp.id}"]`);
-//       if (room) {
-//         const knop = room.querySelector(".js-power-btn");
-//         knop.dataset.statuslamp = lamp.status;
-//         clearClassList(room);
-//         if (lamp.status == 1) {
-//           room.classList.add("c-room--on");
-//         }
-//       }
-//     }
-//   });
+// #region ***  Data Access - get___                     ***********
+const getDevices = function(){
+  const url = 'http://127.0.0.1:5000/api/v1/devices/'
+  handleData(url, showDevices)
+}
+// #endregion
 
-//   socket.on("B2F_verandering_lamp", function (jsonObject) {
-//     console.log("Er is een status van een lamp veranderd");
-//     console.log(jsonObject.lamp.id);
-//     console.log(jsonObject.lamp.status);
+// #region ***  Event Listeners - listenTo___            ***********
+// #endregion
 
-//     const room = document.querySelector(`.js-room[data-idlamp="${jsonObject.lamp.id}"]`);
-//     if (room) {
-//       const knop = room.querySelector(".js-power-btn"); //spreek de room, als start. Zodat je enkel knop krijgt die in de room staat
-//       knop.dataset.statuslamp = jsonObject.lamp.status;
+// #region ***  Init / DOMContentLoaded                  ***********
 
-//       clearClassList(room);
-//       if (jsonObject.lamp.status == 1) {
-//         room.classList.add("c-room--on");
-//       }
-//     }
-//   });
-//   socket.on("B2F_verandering_lamp_from_HRDWR", function (jsonObject) {
-//     console.log(jsonObject)
-//   }) 
+// #endregion
 
-// };
+const init = function(){
+  console.info("DOM ingeladen")
+  console.info("😁")
+  htmlJoystick = document.querySelector('.js-joystick')
+  htmlDevice = document.querySelector('.js-devices')
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   console.info("DOM geladen");
-//   listenToUI();
-//   listenToSocket();
-// });
+  if (htmlDevice){
+    console.info(htmlDevice)
+    getDevices()
+  }
+}
 
-
-
-const listenToSocket = function () {
-  socket.on("connected", function(){
-    console.info("Verbonden met socket webserver")
-  })
-
-
-  // console.info("listentosocket")
-  // socket.on("connected", function () {
-  //   console.log("verbonden met socket webserver");
-  // });
-
-  // socket.on("B2F_devices", function (jsonObject) {
-  //   console.log("Dit zijn alle devices");
-  //   console.log("Dit zijn hun namen");
-  //   console.log(jsonObject);
-  // });
-
-  // socket.on("B2F_verandering_lamp", function (jsonObject) {
-  //   console.log("Er is een status van een lamp veranderd");
-  //   console.log(jsonObject.lamp.id);
-  //   console.log(jsonObject.lamp.status);
-
-  //   const room = document.querySelector(`.js-room[data-idlamp="${jsonObject.lamp.id}"]`);
-  //   if (room) {
-  //     const knop = room.querySelector(".js-power-btn"); //spreek de room, als start. Zodat je enkel knop krijgt die in de room staat
-  //     knop.dataset.statuslamp = jsonObject.lamp.status;
-
-  //     clearClassList(room);
-  //     if (jsonObject.lamp.status == 1) {
-  //       room.classList.add("c-room--on");
-  //     }
-  //   }
-  // });
-  // socket.on("B2F_verandering_lamp_from_HRDWR", function (jsonObject) {
-  //   console.log(jsonObject)
-  // }) 
-};
-
-document.addEventListener('DOMContentLoaded', function(){
-  console.info("DOM geladen")
-  console.info("Hallo! 😃")
-  // listenToUI()
-  listenToSocket()
-});
+document.addEventListener('DOMContentLoaded', init);
 
