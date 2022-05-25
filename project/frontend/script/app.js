5fac34efd3251d54368218b2869b46a3ff032c54
2021-2022-projectone-vinnikalessia <alessia.vinnik@student.howest.be>
2022-05-25 15:35:33 +0200

    nog steeds bezig aan frontend

--- project/frontend/script/app.js
+++ project/frontend/script/app.js
@@ -1,44 +1,44 @@
 "use strict";
 
 // #region ***  DOM references                           ***********
-let htmlDevice, htmlJoystick
+let htmlDevice, htmlJoystick, htmlWaarden
 // #endregion
 
 // #region ***  Callback-Visualisation - show___         ***********
-const showDevices = function(jsonObject){
-  try{
-    console.info(jsonObject)
-    let htmlstring = '';
-    for(const device of jsonObject.devices){
-      htmlstring += `<div class="o-layout__item u-1-of-2-bp3 js-${device.devicenaam}">
-      <h2>${device.devicenaam}:</h2>
-      <div class="c-waarde">
-          waarden x-as: 123
-      </div>
-      <div class="c-waarde">
-          waarden y-as: 321
-      </div>
-      <div class="c-waarde">
-          hoeveel keer er op de knop is gedrukt: 2
-      </div>
-  </div>`;
-    }
-    htmlDevice.innerHTML = htmlstring
-    // getWaarden()
-  }
-  catch (err){
-    console.error(err)
-  }
-}
-
-// const showWaarden = function(jsonObject){
+// const showDevices = function(jsonObject){
 //   try{
 //     console.info(jsonObject)
+//     let htmlstring = '';
+//     for(const device of jsonObject.devices){
+//       htmlstring += `<div class="o-layout__item u-1-of-2-bp3 js-${device.devicenaam}">
+//       <h2>${device.devicenaam}:</h2>
+//       <div class="c-waarde">
+//           waarden x-as: 123
+//       </div>
+//       <div class="c-waarde">
+//           waarden y-as: 321
+//       </div>
+//       <div class="c-waarde">
+//           hoeveel keer er op de knop is gedrukt: 2
+//       </div>
+//   </div>`;
+//     }
+//     htmlDevice.innerHTML = htmlstring
+//   }
+//   catch (err){
+//     console.error(err)
+//   }
+// }
 
+// const showWaarden = function(jsonObject){
+//   console.info(jsonObject)
+//   let htmlstring = ''
+//   for(const waarde of jsonObject.waarden){
+//     htmlstring += `<div class="c-waarde js-waarden">
+//     waarde: ${waarde.waarde}
+// </div>`
 //   }
-//   catch(err){
-//     console.info(err)
-//   }
+//   htmlWaarden.innerHTML = htmlstring
 // }
 
 // #endregion
@@ -47,10 +47,10 @@ const showDevices = function(jsonObject){
 // #endregion
 
 // #region ***  Data Access - get___                     ***********
-const getDevices = function(){
-  const url = 'http://192.168.168.169:5000/api/v1/devices/'
-  handleData(url, showDevices)
-}
+// const getDevices = function(){
+//   const url = 'http://192.168.168.169:5000/api/v1/devices/'
+//   handleData(url, showDevices)
+// }
 
 // const getWaarden = function(){
 //   const url = 'http://192.168.168.169:5000/api/v1/waarden/'
@@ -67,18 +67,18 @@ const getDevices = function(){
 
 const init = function(){
   console.info("😁")
-  // htmlJoystick = document.querySelector('.js-joystick')
+  htmlJoystick = document.querySelector('.js-joystick')
   htmlDevice = document.querySelector('.js-devices')
+  htmlWaarden = document.querySelector('.js-waarden')
 
-  // if (htmlDevice){
-  //   console.info(htmlDevice)
-  //   getDevices()
-  getDevices()
-  
+  // if (htmlWaarden){
+  //   console.info(htmlWaarden)
+  //   getWaarden()
+  // }
 }
 
 // document.addEventListener('DOMContentLoaded', init);
 document.addEventListener('DOMContentLoaded', function(){
   console.info('DOM geladen')
   init()
-})
+});
