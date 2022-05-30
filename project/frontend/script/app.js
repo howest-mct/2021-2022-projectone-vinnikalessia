bffe6863a522ad9782aec9294745ef9e9c3ce8c0
2021-2022-projectone-vinnikalessia <alessia.vinnik@student.howest.be>
2022-05-30 15:26:00 +0200

    waarde joystick x-as verandert, zonder extra tekst

--- project/frontend/script/app.js
+++ project/frontend/script/app.js
@@ -21,36 +21,25 @@ const listenToSocket = function(){
     // tot hier lukt alles
   })
   
-  socket.on('B2F_value_joy_1', function(jsonObject){
-    console.info(jsonObject)
-    let htmlString = ``
-    htmlString += `<div class="o-layout o-layout--gutter-lg js-devices">
-    <div class="o-layout__item u-1-of-2-bp3 js-joystick">
-        <div class="c-waarde js-xwaarde">
-            waarden x-as: ${jsonObject.waarden.waarde}
-        </div>
-        <div class="c-waarde">
-            waarden y-as: 321
-        </div>
-        <div class="c-waarde">
-            hoeveel keer er op de knop is gedrukt: 2
-        </div>
-    </div>
-</div>`
-    console.info("htmlstring")
-    console.info(htmlString)
-    htmlJoystick.innerHTML += htmlString
-    console.info('html aangepast !!!!')
-    // getWaardenJoy()
-    // showWaarden(jsonObject)
-  })
+  try{
+    socket.on('B2F_value_joy_1', function(jsonObject){
+        console.info(jsonObject)
+        let htmlString = ""
+        htmlString += `<div class="c-waarde js-xwaarde">
+        waarden x-as: ${jsonObject.waarden.waarde}
+        </div>`
+        console.info(htmlString)
+        htmlJoystick.innerHTML = htmlString
+        })
+      }catch(err){
+        console.error(err)
+      }
 }
 
 const init = function(){
   console.info("DOM geladen")
   htmlDevices = document.querySelector('.js-devices')
   htmlJoystick = document.querySelector('.js-joystick')
-  htmlXWaarde = document.querySelector('.js-xwaarde')
 
   listenToSocket()
 }
