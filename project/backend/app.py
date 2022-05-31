62b28dd5f85e5b45aa3a2e2820103d3131b0ff6b
2021-2022-projectone-vinnikalessia <alessia.vinnik@student.howest.be>
2022-05-31 16:32:59 +0200

    test met B2F values van joystick 1

--- project/backend/app.py
+++ project/backend/app.py
@@ -17,8 +17,10 @@ global sw_val, x_val, y_val
 
 ########### JOYSTICK ###########
 # deze hangen aan de mcp
-y_as = 0
-x_as = 1
+y_as1 = 0
+x_as1 = 1
+y_as2 = 4
+x_as2 = 5
 
 # de sw van de joystick aan de rpi
 sw = 5
@@ -44,8 +46,7 @@ app.config['SECRET_KEY'] = 'geheim!'
 #socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-socketio = SocketIO(app, cors_allowed_origins="*", logger=False,
-                    engineio_logger=False, ping_timeout=1)
+socketio = SocketIO(app, cors_allowed_origins="*", logger=False, engineio_logger=False, ping_timeout=1)
 
 
 
@@ -67,7 +68,7 @@ def callback_knop(pin):
     global teller, socketio
     teller += 1
     print("Knop joystick 1 is {} keer ingedrukt\n".format(teller))
-    socketio.emit('B2F_value_joy_1', {'teller':teller})
+    socketio.emit('B2F_value_joy_1_sw', {'teller':teller})
     
     # joystick_id(pin)
     return teller
@@ -81,43 +82,40 @@ def joysw_id(sw_id):
     if sw_id == 16:
         commentaar = 'joystick 1 ingedrukt'
         waarde = 1
+        socketio.emit('B2F_value_joy_1_sw', {'teller':teller})
 
     elif sw_id == 19:
         commentaar = 'joystick 2 ingedrukt'
         waarde = 1
+        socketio.emit('B2F_value_joy_2_sw', {'teller':teller})
     return waarde, commentaar
 
 def joystick_id(deviceID):
     if deviceID == 14:
-        # eerste joystick => x-as
         commentaar = "joystick 1 registreerde beweging op x-as"
-        waarde = readChannel(x_as)
-        # socketio.emit('B2F_value_joy_1', {'teller':teller})
-
-        print(f"dit is x: {waarde}")
+        waarde = readChannel(x_as1)
+        socketio.emit('B2F_value_joy_1_x', {'joy_1_x':waarde})
+        print(f"dit is x van joystick 1: {waarde}")
         print(commentaar)
 
     elif deviceID == 15:
-        # eerste joystick => y-as
         commentaar = 'joystick 1 registreerde beweging op y-as'
-        waarde = readChannel(y_as)
-        print(f"dit is y: {waarde}")
+        waarde = readChannel(y_as1)
+        socketio.emit('B2F_value_joy_1_y', {'joy_1_y':waarde})
+        print(f"dit is y van joystick 1: {waarde}")
         print(commentaar)
 
-    # elif joy_id == 17:
-    #     commentaar = 'joystick 2 registreerde beweging op x-as'
-    #     waarde = readChannel(x_as2)
+    elif deviceID == 17:
+        commentaar = 'joystick 2 registreerde beweging op x-as'
+        waarde = readChannel(x_as2)
+        socketio.emit('B2F_value_joy_2_x', {'joy_2_x':waarde})
+        print(f"dit is x van joystick 2: {waarde}")
 
-    # elif joy_id == 18:
-    #     commentaar = 'joystick 2 registreerde beweging op y-as'
-    #     waarde = readChannel(y_as2)
-
-    # elif deviceID == 19:
-    #     commentaar = 'joystick ingedrukt'
-    #     waarde = callback_knop(sw2)
-    #     print(commentaar)
-    #     print(f"dit is de knop: {waarde}")
-
+    elif deviceID == 18:
+        commentaar = 'joystick 2 registreerde beweging op y-as'
+        waarde = readChannel(y_as2)
+        socketio.emit('B2F_value_joy_2_y', {'joy_2_y':waarde})
+        print(f"dit is y van joystick 2: {waarde}")
     return waarde, commentaar
 
 ##################### SOCKETIO #####################
@@ -144,10 +142,10 @@ def joystick(data):
     print(f"joystick met id {joy_id}, heeft de waarde {waarde}")
     DataRepository.create_historiek_joy(joy_id, commentaar, waarde)
 
-    socketio.emit('B2F_value_joy_1', {"waarden":{"deviceid":joy_id, "waarde":waarde}}, broadcast = True)
+    # socketio.emit('B2F_value_joy_1_x', {"waarden":{"deviceid":joy_id, "waarde":waarde}}, broadcast = True)
     time.sleep(0.5)
 
-    # emit("B2F_value_joy_1", {"x_waarde":x_val}, {"y_waarde":y_val}, {"sw_waarde":sw_val}, broadcast = True)
+    # emit("B2F_value_joy_1_x", {"x_waarde":x_val}, {"y_waarde":y_val}, {"sw_waarde":sw_val}, broadcast = True)
     # time.sleep(0.5)
 
 
@@ -238,7 +236,8 @@ def teller_doorsturen():
     while True:
         if teller != last_val:
             print("sending teller")
-            socketio.emit('B2F_value_joy_1', {'teller':teller})
+            # het aantal keren gedrukt op de joystick
+            socketio.emit('B2F_value_joy_1_sw', {'teller':teller})
             last_val = teller
         time.sleep(.5)
 
@@ -267,7 +266,7 @@ def joystick_uitlezen(data):
         y_val = readChannel(y_as)
         print(f"dit is de y: {y_val}\n")
 
-        socketio.emit('B2F_value_joy_1', {"historiek":{"x_as":x_val, "y_as":y_val}})
+        # socketio.emit('B2F_value_joy_1', {"historiek":{"x_as":x_val, "y_as":y_val}})
 
         DataRepository.create_historiek_joy(joy_id, commentaar, waarde) #ToDo
         time.sleep(0.5)
--- project/frontend/script/app.js
+++ project/frontend/script/app.js
@@ -3,7 +3,7 @@
 // const lanIP = `${window.location.hostname}:5000`;
 // const lanIP = `127.0.0.1:5000`;
 const lanIP = `192.168.168.169:5000`;
-const socket = io(`http://${lanIP}`, { transports: ["polling", "websocket"] });
+const socket = io(`http://${lanIP}`, {transports: ["polling", "websocket"] });
 
 let htmlDevices, htmlJoystick, htmlXWaarde
 console.info(lanIP)
@@ -22,23 +22,92 @@ const listenToSocket = function(){
   })
   
 
-  socket.on('B2F_value_joy_1', function(jsonObject){
-      console.info(jsonObject)
-      // ${jsonObject.waarden.waarde}
-      let htmlString = ""
-      htmlString += `<div class="c-waarde js-xwaarde">
-      waarden x-as: 0
-      </div>
-      <div class="c-waarde">
-      waarden y-as: 321
-      </div>
-      <div class="c-waarde">
-      hoeveel keer er op de knop is gedrukt: ${jsonObject.teller}
-      </div>`
-      htmlJoystick.innerHTML = htmlString
-      // tot hier werkt het
-      })
+  // socket.on('B2F_value_joy', function(jsonObject){
+  //     console.info(jsonObject)
+  //     // ${jsonObject.waarden.waarde}
+  //     let htmlString = ""
+  //     htmlString += `<div class="c-waarde js-xwaarde">
+  //     waarden x-as: 0
+  //     </div>
+  //     <div class="c-waarde">
+  //     waarden y-as: 321
+  //     </div>
+  //     <div class="c-waarde">
+  //     hoeveel keer er op de knop is gedrukt: ${jsonObject.teller}
+  //     </div>`
+  //     htmlJoystick.innerHTML = htmlString
+  //     // tot hier werkt het
+  //     })
 
+  //////////////////////////////___TEST___//////////////////////////////
+  // joy 1
+  socket.on('B2F_value_joy_1_sw', function(jsonObject){
+    console.info(jsonObject)
+    // ${jsonObject.waarden.waarde}
+    let htmlString = ""
+    htmlString += `
+    <div class="c-waarde">
+    hoeveel keer er op de knop is gedrukt: ${jsonObject.teller}
+    </div>`
+    htmlJoystick.innerHTML = htmlString
+    })
+
+  socket.on('B2F_value_joy_1_x', function(jsonObject){
+    console.info(jsonObject)
+    // ${jsonObject.waarden.waarde}
+    let htmlString = ""
+    htmlString += `
+    <div class="c-waarde">
+    hoeveel keer er op de knop is gedrukt: ${jsonObject.teller}
+    </div>`
+    htmlJoystick.innerHTML = htmlString
+    })
+
+  socket.on('B2F_value_joy_1_y', function(jsonObject){
+    console.info(jsonObject)
+    // ${jsonObject.waarden.waarde}
+    let htmlString = ""
+    htmlString += `
+    <div class="c-waarde">
+    hoeveel keer er op de knop is gedrukt: ${jsonObject.teller}
+    </div>`
+    htmlJoystick.innerHTML = htmlString
+    })
+
+  // joy 2
+  socket.on('B2F_value_joy_2_sw', function(jsonObject){
+    console.info(jsonObject)
+    // ${jsonObject.waarden.waarde}
+    let htmlString = ""
+    htmlString += `
+    <div class="c-waarde">
+    hoeveel keer er op de knop is gedrukt: ${jsonObject.teller}
+    </div>`
+    htmlJoystick.innerHTML = htmlString
+    })
+
+
+  socket.on('B2F_value_joy_2_x', function(jsonObject){
+    console.info(jsonObject)
+    // ${jsonObject.waarden.waarde}
+    let htmlString = ""
+    htmlString += `
+    <div class="c-waarde">
+    hoeveel keer er op de knop is gedrukt: ${jsonObject.teller}
+    </div>`
+    htmlJoystick.innerHTML = htmlString
+    })
+
+  socket.on('B2F_value_joy_2_y', function(jsonObject){
+    console.info(jsonObject)
+    // ${jsonObject.waarden.waarde}
+    let htmlString = ""
+    htmlString += `
+    <div class="c-waarde">
+    hoeveel keer er op de knop is gedrukt: ${jsonObject.teller}
+    </div>`
+    htmlJoystick.innerHTML = htmlString
+    })
 }
 
 const init = function(){
